37f722be7536c448535d124883997c6a5cfe0252
Prasad Kale <Prasad.Kale@splus.local>
2022-11-28 08:15:17 +0530

    codeadded

---
+++ src/test/java/com/example/indigoms1/IndigoMs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indigoms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndigoMs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
